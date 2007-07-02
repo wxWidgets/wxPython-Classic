da990507f246af53621c6c53903f5da6e2583a6b
Frank Tobia <frank.tobia@gmail.com>
2007-07-02 19:01:05 +0000

    More tests for wx.ComboBox.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxPython/branches/SOC2007_UNITTEST@47084 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- tests/unittest/testComboBox.py
+++ tests/unittest/testComboBox.py
@@ -11,9 +11,8 @@ Methods yet to test:
 
 __init__, CanCopy, CanCut, CanPaste, CanRedo, CanUndo, Copy, Create,
 Cut, GetCurrentSelection, GetInsertionPoint, GetLastPosition, GetMark,
-GetValue, IsEditable, Paste, Redo, Remove, Replace, SelectAll, SetEditable,
-SetInsertionPoint, SetInsertionPointEnd, SetMark, SetStringSelection,
-SetValue, Undo
+Paste, Redo, Remove, Replace, SelectAll, SetInsertionPoint, 
+SetInsertionPointEnd, SetMark, SetStringSelection, Undo
 """
 
 class ComboBoxTest(testChoice.ChoiceTest):
@@ -21,6 +20,29 @@ class ComboBoxTest(testChoice.ChoiceTest):
         self.app = wx.PySimpleApp()
         self.frame = wx.Frame(parent=None, id=wx.ID_ANY)
         self.testControl = wx.ComboBox(parent=self.frame)
+    
+    def testEditable(self):
+        """IsEditable"""
+        self.assert_(self.testControl.IsEditable())
+        self.testControl = wx.ComboBox(self.frame, style=wx.CB_READONLY)
+        self.assert_(not self.testControl.IsEditable())
+    
+    def testSetEditable(self):
+        """SetEditable"""
+        self.testControl.SetEditable(True)
+        self.assert_(self.testControl.IsEditable())
+        self.testControl.SetEditable(False)
+        self.assert_(not self.testControl.IsEditable())
+    
+    def testValue(self):
+        """SetValue, GetValue"""
+        txt1 = "Hello"
+        txt2 = "World"
+        self.testControl.SetValue(txt1)
+        self.assertEquals(txt1, self.testControl.GetValue())
+        self.testControl.SetValue(txt2)
+        self.assertEquals(txt2, self.testControl.GetValue())
+        self.assertNotEquals(txt1, self.testControl.GetValue())
         
 
 def suite():
