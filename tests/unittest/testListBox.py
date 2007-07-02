bed1be215d4b0a1007edcfadbeef0133b860de49
Frank Tobia <frank.tobia@gmail.com>
2007-07-02 18:28:51 +0000

    Tests for wx.ListBox.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxPython/branches/SOC2007_UNITTEST@47080 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- tests/unittest/testListBox.py
+++ tests/unittest/testListBox.py
@@ -5,12 +5,17 @@ import testControlWithItems
 
 """
 This file contains classes and methods for unit testing the API of wx.ListBox.
+
+TODO: fill out docs
         
 Methods yet to test:
-__init__, AppendAndEnsureVisible, Create, Deselect, DeselectAll, EnsureVisible,
-GetSelections, HitTest, Insert, InsertItems, IsSelected,
-IsSorted, Select, Set, SetFirstItem, SetFirstItemStr, SetItemBackgroundColour,
-SetItemFont, SetItemForegroundColour, SetSelection, SetStringSelection
+__init__, AppendAndEnsureVisible, Create, DeselectAll, EnsureVisible,
+GetSelections, HitTest,
+Set, SetFirstItem, SetFirstItemStr, SetStringSelection
+
+These methods don't have corresponding getters:
+SetItemBackgroundColour, SetItemFont, SetItemForegroundColour
+    
 """
 
 class ListBoxTest(testControlWithItems.ControlWithItemsBase):
@@ -18,6 +23,59 @@ class ListBoxTest(testControlWithItems.ControlWithItemsBase):
         self.app = wx.PySimpleApp()
         self.frame = wx.Frame(parent=None, id=wx.ID_ANY)
         self.testControl = wx.ListBox(parent=self.frame)
+    
+    def testDeselect(self):
+        """Deselect"""
+        choices = ['a','b','c','d','e']
+        self.testControl.InsertItems(choices,0)
+        for i in range(len(choices)):
+            self.testControl.Select(i)
+            self.assert_(self.testControl.IsSelected(i))
+            self.testControl.Deselect(i)
+            self.assert_(not self.testControl.IsSelected(i))
+    
+    def testInsertItems(self):
+        """InsertItems"""
+        one = ['a','b','c']
+        two = ['x','y','z']
+        self.testControl.InsertItems(one,0)
+        self.assertEquals(one, self.testControl.GetItems())
+        self.testControl.InsertItems(two,self.testControl.GetCount())
+        self.assertEquals(one+two, self.testControl.GetItems())
+        self.testControl.InsertItems(two,0)
+        self.assertEquals(two+one+two, self.testControl.GetItems())
+    
+    def testSelect(self):
+        """Select, IsSelected"""
+        choices = ['a','b','c','d','e']
+        self.testControl.InsertItems(choices,0)
+        for i in range(len(choices)):
+            self.testControl.Select(i)
+            for j in range(len(choices)):
+                if i == j:
+                    self.assert_(self.testControl.IsSelected(j))
+                else:
+                    self.assert_(not self.testControl.IsSelected(j))
+        
+    def testSetSelection(self):
+        """SetSelection, IsSelected"""
+        choices = ['a','b','c','d','e']
+        self.testControl.InsertItems(choices,0)
+        for i in range(len(choices)):
+            self.testControl.SetSelection(i)
+            for j in range(len(choices)):
+                if i == j:
+                    self.assert_(self.testControl.IsSelected(j))
+                else:
+                    self.assert_(not self.testControl.IsSelected(j))
+    
+    def testSorted(self):
+        """IsSorted"""
+        self.testControl.InsertItems(['a','b','c'],0)
+        self.assert_(self.testControl.IsSorted())
+        self.testControl = wx.ListBox(self.frame, choices=['z','y','x'])
+        self.assert_(not self.testControl.IsSorted())
+        
 
 def suite():
     suite = unittest.makeSuite(ListBoxTest)
