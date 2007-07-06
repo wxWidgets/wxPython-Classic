8070b7b205983afc0e004978c82aa755e791bb9e
Frank Tobia <frank.tobia@gmail.com>
2007-07-06 21:43:51 +0000

    Command-line options for runAllTests.py. Try 'python runAllTests.py --help' for more information.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxPython/branches/SOC2007_UNITTEST@47203 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- tests/unittest/runAllTests.py
+++ tests/unittest/runAllTests.py
@@ -1,9 +1,35 @@
 import os, sys
+from optparse import OptionParser
 
+# Options
+# TODO: would log4py be better than custom flags?
+# TODO: ability to turn off figleaf
+# TODO: store textual output in a file
+# TODO: configure which modules run via command line
+usage = "usage: python %prog [options]"
+parser = OptionParser(usage=usage)
+parser.set_defaults(verbose=True, failure_details=False)
+parser.add_option("-v","--verbose",
+                    action="store_true", dest="verbose",
+                    help="report on each module individually")
+parser.add_option("-q","--quiet",
+                    action="store_false", dest="verbose",
+                    help="only print pass/fail/error totals")
+parser.add_option("-d","--failure-details",
+                    action="store_true", dest="failure_details",
+                    help="print information on each failure"
+                    )
+parser.add_option("-f", "--filename",
+                    action="store", dest="filename",
+                    metavar="FILE", default="tests.figleaf",
+                    help="write figleaf output to FILE [default: %default]"),
+(options, args) = parser.parse_args()
+
+# Figleaf
 rootdir = os.path.abspath(sys.path[0])
 if not os.path.isdir(rootdir):
     rootdir = os.path.dirname(rootdir)
-figfile = os.path.join(rootdir, 'tests.figleaf')
+figfile = os.path.join(rootdir, options.filename)
 
 if os.path.exists(figfile):
     os.remove(figfile)
@@ -38,20 +64,25 @@ for module in modules:
     total_failures  += failures
     total_errors    += errors
     total_successes += successes
-    print "%s:\t%d tests passed" % (module.__name__, successes)
+    if options.verbose:
+        print "%s:\t%d tests passed" % (module.__name__, successes)
     if failures > 0:
-        print "%s:\t%d tests failed!\n" % (module.__name__, failures)
-        for failure in results.failures:
-            print "------ " + str(failure[0]) + " ------"
-            print failure[1]
+        if options.verbose:
+            print "%s:\t%d tests failed!" % (module.__name__, failures)
+        if options.failure_details:
+            for failure in results.failures:
+                print "\n------ " + str(failure[0]) + " ------"
+                print failure[1],
     if errors > 0:
-        print "%s:\t%d tests in error!\n" % (module.__name__, errors)
-        for error in results.errors:
-            print "------ " + str(error[0]) + " ------"
-            print error[1]
+        if options.verbose:
+            print "%s:\t%d tests in error!" % (module.__name__, errors)
+        if options.failure_details:
+            for error in results.errors:
+                print "\n------ " + str(error[0]) + " ------"
+                print error[1],
+if options.verbose or options.failure_details:
+    print "----------------------"
 
-
-print "----------------------"
 print "%d tests passed in total!" % (total_successes)
 if total_failures > 0:
     print "%d tests failed in total!" % (total_failures)
