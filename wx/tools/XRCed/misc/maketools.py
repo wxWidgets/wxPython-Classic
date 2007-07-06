dbfbe2abc1f20c7ddead1221dfd7885ba2da7454
Roman Rolinsky <rolinsky@femagsoft.com>
2007-07-06 18:17:38 +0000

    added more controls and made maketools more robust
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxPython/branches/SOC2007_XRCED@47198 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wx/tools/XRCed/misc/maketools.py
+++ wx/tools/XRCed/misc/maketools.py
@@ -8,34 +8,118 @@ import os,sys
 import wx
 import wx.xrc as xrc
 import wx.grid as grid
-
+import images
 
 def WTF(win, filename):
     """WindowToFile: save part of the screen as 'filename'."""
-    context = wx.ClientDC(win)
-    memory = wx.MemoryDC( )
+    context = wx.ScreenDC()
+    memory = wx.MemoryDC()
     x,y = win.GetPosition()
     w,h = win.GetSize()
+    x0,y0 = win.ClientToScreen((0,0))
+    h += y0 - y + 5
+    w += 10
     bitmap = wx.EmptyBitmap(w, h, -1)
     memory.SelectObject(bitmap)
     memory.Blit(0, 0, w, h, context, x, y)
-    bitmap.SaveFile(filename, wx.BITMAP_TYPE_BMP)
+    bitmap.SaveFile(filename, wx.BITMAP_TYPE_PNG)
     bitmap.Destroy()
     memory.Destroy()
     context.Destroy()
 
-def OnOK(evt):
-    me = evt.GetEventObject()
-    frame = me.GetParent()
-    for w in frame.GetChildren():
-        if w is me: return
+def WCTF(win, dirname):
+    """WindowChildrenToFile: save all child windows."""
+    context = wx.ClientDC(win)
+    memory = wx.MemoryDC()
+    x,y = win.GetPosition()
+    w,h = win.GetSize()
+    bitmap = wx.EmptyBitmap(w, h, -1)
+    memory.SelectObject(bitmap)
+    memory.Blit(0, 0, w, h, context, 0, 0)
+    for w in win.GetChildren():
         klass = w.GetClassName()
+        if w.GetName() != '-1': # replace by the true name
+            klass = w.GetName()
         print 'Saving bitmap for', klass
-        try:
-            WTF(w, os.path.join('bitmaps', klass + '.bmp'))
-        except:
-            print 'Sorry, no luck.'
-    evt.Skip()
+        filename = os.path.join(dirname, klass + '.png')
+        sub = bitmap.GetSubBitmap(w.GetRect())
+        sub.SaveFile(filename, wx.BITMAP_TYPE_PNG)
+        sub.Destroy()
+    bitmap.Destroy()
+    memory.Destroy()
+    context.Destroy()
+
+def create_panels(main_frame):
+    frame = res.LoadFrame(main_frame, 'FRAME_Panels')
+    if not frame:
+        print 'error loading FRAME_Panels'
+        return None
+    # Put some data
+    for w in frame.GetChildren():
+        klass = w.GetClassName()
+        if klass == 'wxTreeCtrl':
+            r = w.AddRoot('Items')
+            w.AppendItem(r, 'Item 1')
+            w.AppendItem(r, 'Item 2')
+            w.Expand(r)
+        elif klass == 'wxListCtrl':
+            w.InsertStringItem(0, "Item 1")
+            w.InsertStringItem(1, "Item 2")
+            w.InsertStringItem(2, "Item 3")
+            w.InsertStringItem(3, "Item 4")
+            w.InsertStringItem(4, "Item 5")
+            w.InsertStringItem(5, "Item 6")
+            w.InsertStringItem(6, "Item 7")
+            w.InsertStringItem(7, "Item 8")
+        elif klass == 'wxGrid':
+            w.CreateGrid(2,1)
+            w.AutoSizeRows()
+            w.AutoSizeColumns()
+        elif klass == 'wxChoicebook':
+            p = wx.ScrolledWindow(w, style=wx.SUNKEN_BORDER)
+            p.SetBackgroundColour(wx.WHITE)
+            w.AddPage(p, 'choice')
+        elif klass == 'wxListbook':
+            il = wx.ImageList(24, 24)
+            imageId = il.Add(images.getListbookPageBitmap())
+            w.AssignImageList(il)
+            p = wx.Panel(w, size=(20,15), style=wx.SUNKEN_BORDER)
+            t = wx.StaticText(p, -1, ' ')
+            p.SetBackgroundColour(wx.WHITE)
+            w.AddPage(p, 'Item1', imageId=imageId)
+            w.AddPage(wx.Panel(w, size=(25,20)), 'Item2')
+            w.AddPage(wx.Panel(w, size=(25,20)), 'Item2')
+            w.AddPage(wx.Panel(w, size=(25,20)), 'Item2')
+        elif klass == 'wxScrolledWindow':
+            w.SetVirtualSize((10,10))
+            w.SetScrollbars(1,1,10,10,1,1)
+            sizer = wx.BoxSizer()
+            p = wx.Panel(w, size=(100,100), style=wx.SUNKEN_BORDER)
+            p.SetBackgroundColour(wx.WHITE)
+            sizer.Add(p)
+            w.SetSizer(sizer)
+    frame.Fit()
+    frame.Show()
+    return frame
+
+def create_controls(main_frame):
+    frame = res.LoadFrame(main_frame, 'FRAME_Controls')
+    if not frame:
+        print 'error loading FRAME_Controls'
+        return None
+    frame.Fit()
+    frame.Show()
+    return frame
+
+def snap(evt):
+    if evt.GetId() == xrc.XRCID('snap_panels'):
+        WCTF(app.frame_panels, 'bitmaps')
+    elif evt.GetId() == xrc.XRCID('snap_controls'):
+        WCTF(app.frame_controls, 'bitmaps')
+    elif evt.GetId() == xrc.XRCID('snap_frame'):
+        WTF(app.frame_frame, 'bitmaps/wxFrame.png')
+    elif evt.GetId() == xrc.XRCID('snap_dialog'):
+        WTF(app.frame_frame, 'bitmaps/wxDialog.png')
 
 if __name__ == '__main__':
     try: 
@@ -43,36 +127,26 @@ if __name__ == '__main__':
     except:
         print 'usage: python maketools.py xrc_file'
         sys.exit(1)
+    global app
     app = wx.PySimpleApp()
     res = xrc.EmptyXmlResource()
     res.Load(resFile)
-    # Frame for controls and other stuff
-    for fname in ['FRAME_Panels', 'FRAME_Controls']:
-        frame = res.LoadFrame(None, fname)
-        if not frame:
-            print 'error loading FRAME'
-        # Put some data
-        for w in frame.GetChildren():
-            klass = w.GetClassName()
-            if klass == 'wxTreeCtrl':
-                r = w.AddRoot('Items')
-                w.AppendItem(r, 'Item 1')
-                w.AppendItem(r, 'Item 2')
-                w.AppendItem(r, 'Item 3')
-                w.AppendItem(r, 'Item 4')
-                w.Expand(r)
-            elif klass == 'wxListCtrl':
-                w.InsertStringItem(0, "Item 1")
-                w.InsertStringItem(1, "Item 2")
-                w.InsertStringItem(2, "Item 3")
-                w.InsertStringItem(3, "Item 4")
-                w.InsertStringItem(4, "Item 5")
-                w.InsertStringItem(5, "Item 6")
-            elif klass == 'wxGrid':
-                w.CreateGrid(2,1)
-                #w.AutoSizeColumns()
-        frame.Fit()
-        frame.Bind(wx.EVT_BUTTON, OnOK, id=wx.ID_OK)
-        frame.Show()
+
+    # Main frame
+    main_frame = res.LoadFrame(None, 'main_frame')
+    assert main_frame
+    app.main_frame = main_frame
+
+    app.frame_panels = create_panels(main_frame)
+    app.frame_controls = create_controls(main_frame)
+
     if not os.path.exists('bitmaps'): os.mkdir('bitmaps')
+
+    main_frame.Bind(wx.EVT_MENU, snap, id=xrc.XRCID('snap_panels'))
+    main_frame.Bind(wx.EVT_MENU, snap, id=xrc.XRCID('snap_controls'))
+    main_frame.Bind(wx.EVT_MENU, snap, id=xrc.XRCID('snap_frame'))
+    main_frame.Bind(wx.EVT_MENU, snap, id=xrc.XRCID('snap_dialog'))
+    main_frame.Bind(wx.EVT_MENU, lambda evt: main_frame.Close(), id=wx.ID_EXIT)
+
+    main_frame.Show()
     app.MainLoop()
