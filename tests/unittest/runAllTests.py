ab40acb092000b52f0ed09a9a6e7ebd4af38a95b
Frank Tobia <frank.tobia@gmail.com>
2007-07-07 16:12:39 +0000

    Add option for wiki-compatible output. More granularity to come.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxPython/branches/SOC2007_UNITTEST@47221 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- tests/unittest/runAllTests.py
+++ tests/unittest/runAllTests.py
@@ -1,6 +1,7 @@
 import os, sys
 import unittest
 import time
+import wx
 from optparse import OptionParser
 
 # ------------------- Helper Methods ------------------------
@@ -18,21 +19,40 @@ def stop_figleaf():
         figleaf.stop()
         figleaf.write_coverage(figfile)
 
-def output_summary():
-    # TODO: add time and duration of test run to output
+def output_summary(wiki=False):
+    if wiki:
+        print "==== Summary ===="
+    if wiki:
+        print " * ",
     print "Run completed in %.2f seconds" % (stop_time-start_time) 
+    if wiki:
+        print " * ",
     print "%d tests passed in total!" % (total_successes)
     if total_failures > 0:
+        if wiki:
+            print " * ",
         print "%d tests failed in total!" % (total_failures)
     if total_errors > 0:
+        if wiki:
+            print " * ",
         print "%d tests erred in total!" % (total_errors)
+
+def wiki_header():
+    if options.wiki:
+        print "=== %s - %s ===" % (time.asctime(),wx.GetOsDescription())
+
+def wiki_platform_info():
+    if options.wiki:
+        print "==== Platform Information ===="
+        print " * '''Platform [sys.platform]''': %s" % (sys.platform)
+        print " * '''Python Version [sys.version]''': %s" % (sys.version)
+        print " * '''wx Version [wx.version()]''': %s" % (wx.version())
+        print " * '''OS [wx.!GetOsDescription()]''': %s" % (wx.GetOsDescription())
         
 # -----------------------------------------------------------
 
 # Options
 # TODO: would log4py be better than custom flags?
-# TODO: ability to turn off figleaf
-# TODO: store textual output in a file
 # TODO: configure which modules run via command line
 usage = "usage: python %prog [options]"
 parser = OptionParser(usage=usage)
@@ -55,6 +75,9 @@ parser.add_option("-f", "--figleaf-filename",
                     action="store", dest="figleaf_filename",
                     metavar="FILE", default="tests.figleaf",
                     help="write figleaf output to FILE [default: %default]")
+parser.add_option("-w", "--wiki",
+                    action="store_true", dest="wiki",
+                    help="write data in wiki-markup format (MoinMoin / wxPyWiki)")
 parser.add_option("-x", "--no-figleaf",
                     action="store_false", dest="figleaf",
                     help="don't use figleaf (code coverage tool)")
@@ -64,6 +87,10 @@ parser.add_option("-x", "--no-figleaf",
 if not options.figleaf and options.figleaf_filename != 'tests.figleaf':
     # not strictly necessary, but -f is useless with -x
     parser.error("options -f and -x are mutually exclusive")
+# may need error-checking, might be a bad idea, but i'm making -w override others
+if options.wiki:
+    options.failure_details = True
+    options.verbose = False
 
 
 rootdir = os.path.abspath(sys.path[0])
@@ -85,10 +112,17 @@ start_figleaf()
 modules = [__import__(f[:-3]) for f in os.listdir(rootdir) 
                 if f.startswith('test') and f.endswith('.py')]
                 
+wiki_header()
+wiki_platform_info()
+# TODO: add a preliminary "here's what I was told to run" output,
+#   and include command-line switches. wiki-only?
+                
 total_successes = 0
 total_failures  = 0
 total_errors    = 0
 start_time = time.time()
+if options.wiki:
+    print "==== Failure Data ===="
 for module in modules:
     # run suite
     suite = module.suite()
@@ -108,20 +142,28 @@ for module in modules:
             print "%s:\t%d tests failed!" % (module.__name__, failures)
         if options.failure_details:
             for failure in results.failures:
-                print "\n------ " + str(failure[0]) + " ------"
-                print failure[1],
+                if options.wiki:
+                    print "\n * " + str(failure[0]).replace('.','.!')
+                    print " {{{" + str(failure[1]) + "}}}"
+                else:
+                    print "\n------ " + str(failure[0]) + " ------"
+                    print failure[1],
     if errors > 0:
         if options.verbose:
             print "%s:\t%d tests in error!" % (module.__name__, errors)
         if options.failure_details:
             for error in results.errors:
-                print "\n------ " + str(error[0]) + " ------"
-                print error[1],
+                if options.wiki:
+                    print "\n * " + str(error[0]).replace('.','.!')
+                    print " {{{" + str(error[1]) + "}}}"
+                else:
+                    print "\n------ " + str(error[0]) + " ------"
+                    print error[1],
 stop_time = time.time()
-if options.verbose or options.failure_details:
+if options.verbose or options.failure_details and not options.wiki:
     print "\n----------------------\n"
 
-output_summary()
+output_summary(options.wiki)
 if origstdout != None:
     sys.stdout = origstdout
     output_summary()
