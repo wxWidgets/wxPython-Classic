824c9ef595b887e957b8d8726094a4694d51f0ab
Roman Rolinsky <rolinsky@femagsoft.com>
2007-09-03 08:48:35 +0000

    unselect tree to keep it from calling selection handler when destroyed
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxPython/branches/SOC2007_XRCED@48528 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wx/tools/XRCed/listener.py
+++ wx/tools/XRCed/listener.py
@@ -327,6 +327,7 @@ class _Listener:
                 if conf.showToolPanel and self.toolFrame:
                     conf.toolPanelPos = self.toolFrame.GetPosition()
                     conf.toolPanelSize = self.toolFrame.GetSize()
+        self.tree.UnselectAll()        
         #self.panel.Destroy()            # destroy panel before tree
         self.frame.Destroy()
 
@@ -589,8 +590,6 @@ Homepage: http://xrced.sourceforge.net\
 
         # Check clipboard
         self.clipboardHasData = True
-        self.inIdle = False
-        return
 #        self.clipboardHasData = False
         wx.TheClipboard.Flush()
         cbLock = wx.ClipboardLocker()
