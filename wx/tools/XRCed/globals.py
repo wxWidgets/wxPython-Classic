e01572bbed3d791488a14455688adbd182d7bfe1
Roman Rolinsky <rolinsky@femagsoft.com>
2007-10-25 21:56:25 +0000

    status upgrade to beta version
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxPython/branches/SOC2007_XRCED@49440 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- wx/tools/XRCed/globals.py
+++ wx/tools/XRCed/globals.py
@@ -20,7 +20,7 @@ logger = logging.getLogger('xrced')
 
 progname = 'xrced'
 ProgName = 'XRCed'
-version = '0.2.0-alpha'
+version = '0.2.0-beta'
 # Minimal wxWidgets version
 MinWxVersion = (2,8,0)
 if wx.VERSION[:3] < MinWxVersion:
