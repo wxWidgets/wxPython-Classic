af27a63a1a734d8dedb2be23bb673680c5ba0f35
Robin Dunn <robin@alldunn.com>
2007-12-06 00:04:56 +0000

    Merge r49047 from 2.8 branch
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxPython/trunk@50493 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- demo/Main.py
+++ demo/Main.py
@@ -877,9 +877,9 @@ class DemoModules:
     def LoadDict(self, modID):
         if self.name != __name__:
             source = self.modules[modID][1]
-            #description = self.modules[modID][3]
             description = self.modules[modID][2]
-
+            description = description.encode(sys.getfilesystemencoding())
+            
             try:
                 self.modules[modID][0] = {}
                 code = compile(source, description, "exec")        
