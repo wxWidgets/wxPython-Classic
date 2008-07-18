733660f09f08375ccb32a4dac30f939a2a400e6e
Robin Dunn <robin@alldunn.com>
2008-07-18 03:29:47 +0000

    Also reset self.parent when PyOnDemandOutputWindow closes
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxPython/branches/WX_2_8_BRANCH@54680 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/_app_ex.py
+++ src/_app_ex.py
@@ -48,6 +48,7 @@ class PyOnDemandOutputWindow:
             self.frame.Destroy()
         self.frame = None
         self.text  = None
+        self.parent = None
 
 
     # These methods provide the file-like output behaviour.
