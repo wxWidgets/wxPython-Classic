7aeef65fee0e09ad586cb777f790577420cedcdf
Robin Dunn <robin@alldunn.com>
2008-08-08 06:24:36 +0000

    Merge r55028 from 2.8 branch
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxPython/trunk@55029 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- distrib/msw/.makesetup.mk
+++ distrib/msw/.makesetup.mk
@@ -1,18 +1,27 @@
 
+
+HYB_SEDCMD=sed "s!wxUSE_MEMORY_TRACING 1!wxUSE_MEMORY_TRACING 0!g;s!wxUSE_DEBUG_CONTEXT 1!wxUSE_DEBUG_CONTEXT 0!g"
+!if "$(CPU)" == "AMD64"
+UNI_SEDCMD=sed "s!wxUSE_UNICODE 0!wxUSE_UNICODE 1!g"
+DLLDIR=vc_amd64_dll
+!else
+UNI_SEDCMD=sed "s!wxUSE_UNICODE 0!wxUSE_UNICODE 1!g;s!wxUSE_UNICODE_MSLU 0!wxUSE_UNICODE_MSLU 1!g"
+DLLDIR=vc_dll
+!endif
+
+
 SRC=$(WXWIN)\include\wx\msw\setup.h
 DIR=$(WXWIN)\lib
-FILES=	$(DIR)\vc_dll\mswd\wx\setup.h \
-        $(DIR)\vc_dll\mswh\wx\setup.h \
-        $(DIR)\vc_dll\mswud\wx\setup.h \
-        $(DIR)\vc_dll\mswuh\wx\setup.h \
-        $(DIR)\vc_dll\msw\wx\setup.h \
-        $(DIR)\vc_dll\mswu\wx\setup.h \
+FILES=	$(DIR)\$(DLLDIR)\mswd\wx\setup.h \
+        $(DIR)\$(DLLDIR)\mswh\wx\setup.h \
+        $(DIR)\$(DLLDIR)\mswud\wx\setup.h \
+        $(DIR)\$(DLLDIR)\mswuh\wx\setup.h \
+        $(DIR)\$(DLLDIR)\msw\wx\setup.h \
+        $(DIR)\$(DLLDIR)\mswu\wx\setup.h \
 	\
         $(DIR)\vc_lib\msw\wx\setup.h \
 
 
-UNI_SEDCMD=sed "s!wxUSE_UNICODE 0!wxUSE_UNICODE 1!g;s!wxUSE_UNICODE_MSLU 0!wxUSE_UNICODE_MSLU 1!g"
-HYB_SEDCMD=sed "s!wxUSE_MEMORY_TRACING 1!wxUSE_MEMORY_TRACING 0!g;s!wxUSE_DEBUG_CONTEXT 1!wxUSE_DEBUG_CONTEXT 0!g"
 
 all : $(FILES)
 
@@ -20,18 +29,18 @@ test :
 	echo $(DIR)\vc_lib\msw\wx\setup.h
 
 # debug
-$(DIR)\vc_dll\mswd\wx\setup.h : $(SRC) .makesetup.mk
-	-if not exist  $(DIR)\vc_dll\mswd\wx mkdir $(DIR)\vc_dll\mswd\wx
+$(DIR)\$(DLLDIR)\mswd\wx\setup.h : $(SRC) .makesetup.mk
+	-if not exist  $(DIR)\$(DLLDIR)\mswd\wx mkdir $(DIR)\$(DLLDIR)\mswd\wx
 	cat $(SRC) > $@
 
 # hybrid
-$(DIR)\vc_dll\mswh\wx\setup.h : $(SRC) .makesetup.mk
-	-if not exist  $(DIR)\vc_dll\mswh\wx mkdir $(DIR)\vc_dll\mswh\wx
+$(DIR)\$(DLLDIR)\mswh\wx\setup.h : $(SRC) .makesetup.mk
+	-if not exist  $(DIR)\$(DLLDIR)\mswh\wx mkdir $(DIR)\$(DLLDIR)\mswh\wx
 	cat $(SRC) | $(HYB_SEDCMD) > $@
 
 # release
-$(DIR)\vc_dll\msw\wx\setup.h : $(SRC) .makesetup.mk
-	-if not exist  $(DIR)\vc_dll\msw\wx mkdir $(DIR)\vc_dll\msw\wx
+$(DIR)\$(DLLDIR)\msw\wx\setup.h : $(SRC) .makesetup.mk
+	-if not exist  $(DIR)\$(DLLDIR)\msw\wx mkdir $(DIR)\$(DLLDIR)\msw\wx
 	cat $(SRC) > $@
 
 $(DIR)\vc_lib\msw\wx\setup.h : $(SRC) .makesetup.mk
@@ -39,18 +48,18 @@ $(DIR)\vc_lib\msw\wx\setup.h : $(SRC) .makesetup.mk
 	cat $(SRC) > $@
 
 # debug-uni
-$(DIR)\vc_dll\mswud\wx\setup.h : $(SRC) .makesetup.mk
-	-if not exist  $(DIR)\vc_dll\mswud\wx mkdir $(DIR)\vc_dll\mswud\wx
+$(DIR)\$(DLLDIR)\mswud\wx\setup.h : $(SRC) .makesetup.mk
+	-if not exist  $(DIR)\$(DLLDIR)\mswud\wx mkdir $(DIR)\$(DLLDIR)\mswud\wx
 	cat $(SRC) | $(UNI_SEDCMD) > $@
 
 # hybrid-uni
-$(DIR)\vc_dll\mswuh\wx\setup.h : $(SRC) .makesetup.mk
-	-if not exist  $(DIR)\vc_dll\mswuh\wx mkdir $(DIR)\vc_dll\mswuh\wx
+$(DIR)\$(DLLDIR)\mswuh\wx\setup.h : $(SRC) .makesetup.mk
+	-if not exist  $(DIR)\$(DLLDIR)\mswuh\wx mkdir $(DIR)\$(DLLDIR)\mswuh\wx
 	cat $(SRC) | $(UNI_SEDCMD) | $(HYB_SEDCMD) > $@
 
 # release-uni
-$(DIR)\vc_dll\mswu\wx\setup.h : $(SRC) .makesetup.mk
-	-if not exist  $(DIR)\vc_dll\mswu\wx mkdir $(DIR)\vc_dll\mswu\wx
+$(DIR)\$(DLLDIR)\mswu\wx\setup.h : $(SRC) .makesetup.mk
+	-if not exist  $(DIR)\$(DLLDIR)\mswu\wx mkdir $(DIR)\$(DLLDIR)\mswu\wx
 	cat $(SRC) | $(UNI_SEDCMD) > $@
 
 
