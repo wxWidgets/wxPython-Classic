2aed86fb37163b833349d7fd63bd7b94ebf3c8bf
Robin Dunn <robin@alldunn.com>
2010-10-05 23:37:22 +0000

    Fix lib version string for 64-bit build
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxPython/trunk@65772 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- distrib/make_installer.py
+++ distrib/make_installer.py
@@ -737,7 +737,7 @@ def find_DLLs():
     if os.environ.get('CPU', '') == 'AMD64':
         # Just hard-code it for now until a good solution for finding
         # the right dumpbin can be found...
-        return '290uh', sys.version[:3]
+        return '291u', sys.version[:3]
         
     WXDLLVER = PYTHONVER = None
 
