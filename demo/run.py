c309299dadf5e90c7117361a81ec339775cf5adc
Robin Dunn <robin@alldunn.com>
2011-08-25 18:45:24 +0000

    Use InitInspection
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxPython/trunk@68888 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- demo/run.py
+++ demo/run.py
@@ -54,7 +54,7 @@ class RunDemoApp(wx.App, wx.lib.mixins.inspection.InspectionMixin):
         wx.Log.SetActiveTarget(wx.LogStderr())
 
         self.SetAssertMode(assertMode)
-        self.Init()  # InspectionMixin
+        self.InitInspection()  # for the InspectionMixin base class
 
         frame = wx.Frame(None, -1, "RunDemo: " + self.name, pos=(50,50), size=(200,100),
                         style=wx.DEFAULT_FRAME_STYLE, name="run a sample")
