e0e0759a7a8430cea92fb50e1d2369fae88c7e67
Robin Dunn <robin@alldunn.com>
2011-09-03 04:25:32 +0000

    assert that the callables passed to wx.CallAfter and wx.CallLater are actually callable
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxPython/trunk@68984 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/_core_ex.py
+++ src/_core_ex.py
@@ -144,7 +144,7 @@ class _wxPyUnbornObject(object):
 
 #----------------------------------------------------------------------------
 
-def CallAfter(callable, *args, **kw):
+def CallAfter(callableObj, *args, **kw):
     """
     Call the specified function after the current and pending event
     handlers have been completed.  This is also good for making GUI
@@ -153,6 +153,7 @@ def CallAfter(callable, *args, **kw):
 
     :see: `wx.CallLater`
     """
+    assert callable(callableObj), "callableObj is not callable"
     app = wx.GetApp()
     assert app is not None, 'No wx.App created yet'
 
@@ -162,7 +163,7 @@ def CallAfter(callable, *args, **kw):
                     lambda event: event.callable(*event.args, **event.kw) )
     evt = wx.PyEvent()
     evt.SetEventType(app._CallAfterId)
-    evt.callable = callable
+    evt.callable = callableObj
     evt.args = args
     evt.kw = kw
     wx.PostEvent(app, evt)
@@ -186,9 +187,10 @@ class CallLater:
 
     :see: `wx.CallAfter`
     """
-    def __init__(self, millis, callable, *args, **kwargs):
+    def __init__(self, millis, callableObj, *args, **kwargs):
+        assert callable(callableObj), "callableObj is not callable"
         self.millis = millis
-        self.callable = callable
+        self.callable = callableObj
         self.SetArgs(*args, **kwargs)
         self.runCount = 0
         self.running = False
