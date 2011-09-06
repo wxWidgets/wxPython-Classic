28351695716674e736417e0f39954399d8713988
Robin Dunn <robin@alldunn.com>
2011-09-06 04:21:03 +0000

    reSWIGged
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxPython/trunk@69010 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- src/msw/_core.py
+++ src/msw/_core.py
@@ -16302,7 +16302,6 @@ if RELEASE_VERSION != _core_.RELEASE_VERSION:
 
 def version():
     """Returns a string containing version and port info"""
-    ctype = wx.USE_UNICODE and 'unicode' or 'ansi'
     if wx.Platform == '__WXMSW__':
         port = 'msw'
     elif wx.Platform == '__WXMAC__':
@@ -16317,8 +16316,8 @@ def version():
     else:
         port = '?'
 
-    return "%s (%s-%s)" % (wx.VERSION_STRING, port, ctype)
-                       
+    return "%s %s (classic)" % (wx.VERSION_STRING, port)
+                      
     
 #----------------------------------------------------------------------------
 
