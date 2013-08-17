194ad47f9e79fc671f4c5f38b10dd43683545ea9
Robin Dunn <robin@alldunn.com>
2013-08-17 20:28:58 +0000

    Updated wx.lib.pdfviewer with patches from David Hughes.
    git-svn-id: https://svn.wxwidgets.org/svn/wx/wxPython/trunk@74668 c3d73ce0-8a6f-49c7-b76d-6d57e0e08775

--- demo/PDFViewer.py
+++ demo/PDFViewer.py
@@ -1,12 +1,19 @@
 
 import  wx
-
+have_package = True
 try:
-    import pyPdf
+    import fitz
+except ImportError:    
+    try:
+        import PyPDF2
+    except ImportError:
+        try:
+            import pyPdf
+        except ImportError:
+            have_package = False
+
+if have_package:            
     from wx.lib.pdfviewer import pdfViewer, pdfButtonPanel
-    havePyPdf = True
-except ImportError:
-    havePyPdf = False
 
 #----------------------------------------------------------------------
 
@@ -46,15 +53,15 @@ class TestPanel(wx.Panel):
 #----------------------------------------------------------------------
 
 def runTest(frame, nb, log):
-    if havePyPdf:
+    if have_package:
         win = TestPanel(nb, log)
         return win
     else:
         from Main import MessagePanel
-        win = MessagePanel(nb, 
-                           'This demo requires the pyPdf package to be installed.\n'
-                           'See: http://pybrary.net/pyPdf/', 
-                           'Sorry', wx.ICON_WARNING)
+        win = MessagePanel(nb,
+                      'Either Python-fitz (mupdf) or PyPDF2 or pyPdf package'+
+                      ' must be available for this demo to run',
+                          'Sorry', wx.ICON_WARNING)
         return win
 
 overview = """\
@@ -65,9 +72,16 @@ The wx.lib.pdfviewer.pdfViewer class is derived from wx.ScrolledWindow
 and can display and print PDF files. The whole file can be scrolled from
 end to end at whatever magnification (zoom-level) is specified.
 
-<p> The viewer uses <b>pyPdf</b> to parse the pdf file so it is a requirement that
-this must be installed. The pyPdf home page is http://pybrary.net/pyPdf/
-and the library can also be downloaded from http://pypi.python.org/pypi/pyPdf/1.12
+<p> The viewer uses mupdf library (via Python-fitz bindings) as
+the backend PDF extraction & rendering engine if installed 
+otherwise PyPdf2 else pyPdf. The packages are available from
+                           <p>https://github.com/rk700/python-fitz
+                           <p>http://www.mupdf.com
+                           <p>http://knowah.github.io/PyPDF2
+                           <p>http://pypi.python.org/pypi/pyPdf/1.12
+
+<p>WARNING. mupdf is GPL so be wary of distributing this in a frozen app
+using py2exe, py2app or similar.
 
 <p> There is an optional pdfButtonPanel class, derived from wx.lib.agw.buttonpanel,
 that can be placed, for example, at the top of the scrolled viewer window,
@@ -75,17 +89,19 @@ and which contains navigation and zoom controls.
 
 <p>Alternatively you can drive the viewer from controls in your own application.
 Externally callable methods are: LoadFile, Save, Print, SetZoom, and GoPage.
-(See pdfviewer.__init__.py)
 
 <p> The viewer renders the pdf file content using Cairo if installed,
 otherwise wx.GraphicsContext is used. Printing is achieved by writing
-directly to a wx.PrintDC and using wx.Printer.
+to a wx.PrintDC and using wx.Printer.
 
-<p> Please note that pdfviewer is a far from complete implementation of the pdf
-specification and will probably fail to display any random file you supply. 
+<p> Please note that when pdfviewer uses pyPDF2 or pyPdf as a back-end, it is a far from
+complete implementation of the pdf specification and will probably fail to display
+any random file you supply. 
 However it does seem to be OK with the sort of files produced by ReportLab that
-use Western languages. The biggest limitation is probably that it doesn't (yet?)
-support embedded fonts and will substitute one of the standard fonts instead.
+use Western languages. The biggest limitation is probably that it doesn't 
+support embedded fonts and will substitute one of the standard fonts instead. This may lead to
+problems if the embedded font used a different encoding. The mupdf back-end is both
+complete and fast but note the GPL licence warning above.
 
 </body></html>
 """
